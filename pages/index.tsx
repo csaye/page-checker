ef01f4f68897e6d350c1ed67293da862f033e61d
Cooper Saye <coopersaye@gmail.com>
2023-10-28 21:35:13 -0400

    Add text mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import styles from '@/styles/pages/Index.module.scss'
 import { useState } from 'react'
 import { CheckPageResponse } from './api/checkPage'
-import { html } from 'js-beautify'
+import { html as formatHtml } from 'js-beautify'
+
+type OutputType = 'text' | 'html'
 
 export default function Index() {
   const [url, setUrl] = useState('')
+  const [type, setType] = useState<OutputType>('text')
   const [text, setText] = useState('')
 
   return (
@@ -21,12 +24,21 @@ export default function Index() {
           placeholder='https://example.com'
           required
         />
+
+        <select
+          value={type}
+          onChange={(e) => setType(e.target.value as OutputType)}
+        >
+          <option value='text'>Text</option>
+          <option value='html'>HTML</option>
+        </select>
+
         <button>Get Page</button>
       </form>
 
       {text && (
         <pre>
-          <code>{html(text)}</code>
+          <code>{text}</code>
         </pre>
       )}
     </div>
@@ -40,6 +52,19 @@ export default function Index() {
       return
     }
     const json = (await response.json()) as CheckPageResponse
-    setText(json.text)
+    const html = json.text
+
+    if (type === 'html') {
+      setText(formatHtml(html))
+      return
+    }
+
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(html, 'text/html')
+    const cleanText = (doc.body.textContent?.split('\n') ?? [])
+      .map((text) => text.trim())
+      .filter((text) => !!text)
+      .join('\n')
+    setText(cleanText)
   }
 }
